beae9e5d257eafa08112eeb8767393a37fcf7f59
Abhinav Kumar Choudhary <abhinav.kumar.1264@gmail.com>
2025-10-29 00:36:09 +0530

    db migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251028_185708 from './20251028_185708';
+
+export const migrations = [
+  {
+    up: migration_20251028_185708.up,
+    down: migration_20251028_185708.down,
+    name: '20251028_185708'
+  },
+];
